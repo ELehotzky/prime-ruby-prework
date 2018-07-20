682656988c2725226c7193363b4127603bcf35fc
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-20 01:57:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,59 @@
-# Add  code here!
-
-def prime?(num)
-  if num < 1 
-    return false 
-    elsif loop do (var i = 2; i < num; i++) 
-      return true 
-    end
+def prime?(integer)
+  if integer < 2 
+    false
+  elsif integer == 2 || integer == 3 
+    true
+  elsif integer % 2 == 0 || integer % 3 == 0 
+    false
   end
-  
-  
-  
-  
-  ## function isPrime(value) {
-    for(var i = 2; i < value; i++) {
-        if(value % i === 0) {
-            return false;
-        }
-    }
-    return value > 1;
-}
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Add  code here!
+
+# def prime?(num)
+#   if num < 2 
+#     return false 
+#   elsif num == 2 
+#   return true 
+# end
+#   i = 2...num 
+#   for item in i 
+#   if num % item == 0 
+#     return false 
+#   end 
+#   true 
+# end
+# end
